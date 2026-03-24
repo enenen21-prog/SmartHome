0459a874a1419bdc2726971819f5fac803690b3e
enenen21-prog <enenen21@gmail.com>
2026-03-24 07:07:53 +0200

    Change file structure

--- backend/MyAPi/obj/Debug/net10.0/MyApi.AssemblyInfo.cs
+++ backend/MyAPi/obj/Debug/net10.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a97842f1f223b7e00cef1bcdcd540926c84f08e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b090bd6212e65094737636efa0df15248ff0574")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
